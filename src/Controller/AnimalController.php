1af2bd44f21133ef60e72bee4764aa8c785f40bc
Alex Saunders <alex.saunders@digital.justice.gov.uk>
2019-09-20 11:33:55 +0100

    adds initial controller

---
+++ src/Controller/AnimalController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnimalController extends AbstractController
+{
+    /**
+     * @Route("/animal", name="animal")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AnimalController.php',
+        ]);
+    }
+}
